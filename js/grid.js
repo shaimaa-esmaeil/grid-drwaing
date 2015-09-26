3a535a30900f46c9dd3a0d981df3be98a324c5c5
shaimaa-esmaeil <shaimaa.azmi@gmail.com>
2015-09-26 14:51:01 +0200

    finished the buttons

--- js/grid.js
+++ js/grid.js
@@ -9,7 +9,38 @@ $(document).ready(function()
 		//$("#square").hover(function(){
 		//	$(this).css("background-color","black");
 
-		//});
+		mouseRespond();
+		$('#new').click(function()
+		{
+			$(".selected").removeClass("selected");
+			console.log("click");
+			var gridSize = prompt("Pleas enter the grid size:","");
+			console.log(gridSize);
+			$('.wrapper').empty();
+
+			for (var i = 0; i<=gridSize; i++)
+				{	
+				
+				$(".wrapper").append('<div class = "square">'+ i +'</div>');
+				console.log(gridSize);
+				}
+			mouseRespond();
+
+			});
+		$('#erase').click(function()
+		{
+			$(".selected").removeClass("selected");
+		});
+		
+
+
+	
+
+
+});
+
+
+var mouseRespond = function(){
 		$(".square").mouseenter(function()
 		{
 			$(this).addClass("square");
@@ -19,12 +50,4 @@ $(document).ready(function()
 		{
 			$(this).addClass("selected");
 
-		});
-
-		
-
-
-	
-
-
-});
+		});};
